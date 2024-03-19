51242434362e458eb10709d3a8328243530c26ad
jonghyun0429p <bwhdgus0429@naver.com>
2024-03-20 01:40:30 +0900

    pemCheck2

---
+++ jong/src/main/java/awsreactspring/jong/JongApplication.java
@@ -0,0 +1,13 @@
+package awsreactspring.jong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JongApplication.class, args);
+	}
+
+}
---
+++ jong/src/test/java/awsreactspring/jong/JongApplicationTests.java
@@ -0,0 +1,13 @@
+package awsreactspring.jong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
